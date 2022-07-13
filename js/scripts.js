857b97fc789d2a134e8ab800e12699884d1608b2
Julieta Glenadel <jbglenadel@gmail.com>
2022-07-13 14:59:41 -0300

    Front-end  Challenge

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,47 @@
 /* example function name */
-const openAndCloseAccordion = () => {
-  alert ('do something')
+// alert("Hello! I am an alert box!!");
+
+// NavBar
+function classToggle() {
+  const navs = document.querySelectorAll('.navbar__items')
+  
+  navs.forEach(nav => nav.classList.toggle('navbar__toggleShow'));
+}
+
+document.querySelector('.navbar__toggle')
+  .addEventListener('click', classToggle);
+
+// FAQs acordion
+
+const faqs = document.querySelectorAll(".faq__content");
+
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
+  })
+})
+
+// sliders
+
+let slideIndex = 1;
+showSlides(slideIndex);
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
